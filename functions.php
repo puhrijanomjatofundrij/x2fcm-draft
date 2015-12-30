46411884f59fc867360d10efe3d99f01fa225604
puhrijanomjatofundrij <inf.srv01@gmail.com>
2015-12-30 13:20:17 +0200

    functions.php - Update

--- functions.php
+++ functions.php
@@ -1,19 +1,23 @@
 <?php
-require_once('config.php');
-class xfcm
-{
-	
-	public $request = $_SESSION['REQUESTED_URI'];
-	
-	public function start()
+if(defined('INGRESS')):
+	require_once('config.php');
+	class xfcm
 	{
-		require_once('/database/pages.php');
-		if (!empty($file)):
-			require_once('memory/'.$file.'.php');
-		else:
-			require_once('memory/404.php');
-		endif;
-	}
 	
-}
+		public $request = $_SESSION['REQUESTED_URI'];
+	
+		public function start()
+		{
+			require_once('/database/pages.php');
+			if (!empty($file)):
+				require_once('memory/'.$file.'.php');
+			else:
+				require_once('memory/404.php');
+			endif;
+		}
+	
+	}
+else:
+	header('Location: 404');
+endif;
 ?>
