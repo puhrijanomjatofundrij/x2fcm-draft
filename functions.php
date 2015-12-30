3093ea2636736e3018b08a990e19f54173429a2a
puhrijanomjatofundrij <inf.srv01@gmail.com>
2015-12-30 13:14:31 +0200

    functions.php - Update
    Update functions.php

--- functions.php
+++ functions.php
@@ -3,9 +3,16 @@ require_once('config.php');
 class xfcm
 {
 	
-	function start()
+	public $request = $_SESSION['REQUESTED_URI'];
+	
+	public function start()
 	{
-		/* code is here */
+		require_once('/database/pages.php');
+		if (!empty($file)):
+			require_once('memory/'.$file.'.php');
+		else:
+			require_once('memory/404.php');
+		endif;
 	}
 	
 }
